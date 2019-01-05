121e89d4bbdad63cc13250922e50ed9c81608754
Bethy Robles <bethy.robles13@gmail.com>
2019-01-05 17:28:41 -0300

    assigment1 rev0

---
+++ assignment1.js
@@ -0,0 +1,8 @@
+function calculaAngulo (hora,min){
+	If (hora>12) {hora -= 12}
+	var calHora = hora * 5 + min * 5 / 60;
+	var calMin = calHora - min;
+	var angulo = Calmin * 360/60;
+	return angulo;
+
+}
